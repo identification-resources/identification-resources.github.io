170527881008d7717fb503d34600ab3b7235aa08
Lars Willighagen <lars.willighagen@gmail.com>
2022-08-11 23:24:04 +0200

    feat(detail): link to resources

--- assets/js/detail.js
+++ assets/js/detail.js
@@ -62,6 +62,22 @@
     ))
     document.getElementById('complete').textContent = data.complete ? data.complete === 'TRUE' ? 'Yes' : 'No' : ''
 
+    {
+        const element = document.getElementById('resources')
+        const keys = await loadKeys()
+        let keyIndex = 1
+        let keyId
+        while ((keyId = `${id}:${keyIndex}`) in keys) {
+            const key = keys[keyId]
+            const a = document.createElement('a')
+            a.setAttribute('href', `/catalog/resource/?id=${keyId}`)
+            a.textContent = `${keyId} (${key.taxonCount} taxa)`
+            if (keyIndex > 1) { element.append(', ') }
+            element.appendChild(a)
+            keyIndex++
+        }
+    }
+
     // Bibliographical info
     if (data.entry_type) {
         if (octicons[data.entry_type]) {
